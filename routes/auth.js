c71911f3d4e88b16474bd91e5bfc1261c9e59727
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-03 17:43:20 +0530

    Added Validation to Different fields in the route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,29 @@
 const { Router } = require('express')
 
-const {} = require('../controllers/auth')
+const { signup } = require('../controllers/auth')
 
 const { body } = require('express-validator/check')
-
+const User = require('../models/user')
 const router = Router()
 
-router.put('/signup')
+router.put(
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please Enter a Valid Email')
+			.custom(async (emailVal, { req }) => {
+				if (!(await User.findOne({ email: emailVal }))) {
+					throw new Error('Email already exists...')
+				}
+				return true
+			})
+			.normalizeEmail(),
+
+		body('password').trim().isLength({ min: 5 }),
+		body('name').trim().not().isEmpty(),
+	],
+	signup
+)
 
 module.exports = router
