12d981958b2b58baaaebe9c968bc381abed01436
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-03 18:03:16 +0530

    Hashed pass  and stored in User collection

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const { validationResult } = require('express-validator/check')
+const bcrypt = require('bcryptjs')
 
 exports.signup = async (req, res, next) => {
 	try {
@@ -12,6 +13,20 @@ exports.signup = async (req, res, next) => {
 		}
 
 		const { name, email, password } = { ...req.body }
+		const hashedPassword = await bcrypt.hash(password, 12)
+
+		const user = new User({
+			name,
+			email,
+			password: hashedPassword,
+		})
+
+		const result = await user.save()
+		return res.status(201).json({
+			message: 'User was created Successfully!',
+			userId: result._id,
+			user: result,
+		})
 	} catch (err) {
 		if (!err.statusCode) {
 			err.statusCode = 500
